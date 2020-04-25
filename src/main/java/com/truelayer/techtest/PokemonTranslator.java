37b3dc9e13ffc68144c3240e13252a510d229013
danielmcerlean <dmcerlean1@googlemail.com>
2020-04-25 12:03:03 +0100

    Initial commit - project creation with auto-generated classes

---
+++ src/main/java/com/truelayer/techtest/PokemonTranslator.java
@@ -0,0 +1,13 @@
+package com.truelayer.techtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonTranslator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonTranslator.class, args);
+    }
+
+}
---
+++ src/test/java/com/truelayer/techtest/PokemonTranslatorTests.java
@@ -0,0 +1,13 @@
+package com.truelayer.techtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTranslatorTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
